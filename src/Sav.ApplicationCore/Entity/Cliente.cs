919843dda9838cd4e22bcb956897cf40d4136ed6
carlos <carlos@igal.matriz>
2018-06-14 10:46:30 -0300

    Add project files.

---
+++ src/Sav.ApplicationCore/Entity/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sav.ApplicationCore.Entity
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string DocumentoId { get; set; }
+
+    }
+}
---
+++ src/Sav.ApplicationCore/Entity/Documento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sav.ApplicationCore.Entity
+{
+    public class Documento
+    {
+        public Documento()
+        {
+
+        }
+        public int Id { get; set; }
+        public string CPF { get; set; }
+        public string InscricaoEstadual { get; set; }
+        public string InscricaoMunicipal { get; set; }
+        public string Cnpj { get; set; }
+    }
+}
